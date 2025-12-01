37e9dd0b9ff951a9756153b4ba780b020f23b141
nafisuddinn <155024278+nafisuddinn@users.noreply.github.com>
2025-12-01 02:54:16 -0500

    Add files via upload
    Models + Schemas for the new endpoints

--- app/models.py
+++ app/models.py
@@ -14,8 +14,9 @@ Later we can add:
 """
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, Integer, Float, Text, Boolean, ForeignKey, CheckConstraint
+from sqlalchemy import String, Integer, Float, Text, Boolean, ForeignKey, CheckConstraint, DateTime, func, UniqueConstraint
 from .db import Base
+from datetime import datetime #for photos when photos get uploaded
 
 
 class User(Base): # actual creation of a user comes in auth
@@ -32,6 +33,19 @@ class User(Base): # actual creation of a user comes in auth
 
     # Display name -- CookinUpMemez would be tuff here
     display_name: Mapped[str] = mapped_column(String)
+
+    notes: Mapped[list["Note"]] = relationship(back_populates="user", cascade="all, delete-orphan")
+
+    favorites: Mapped[list["Favorite"]] = relationship(back_populates="user", cascade="all, delete-orphan")
+
+    activities: Mapped[list["Activity"]] = relationship(back_populates="user", cascade="all, delete-orphan")
+
+    profile: Mapped[list["Profile"]] = relationship(back_populates="user", cascade="all, delete-orphan", uselist=False)
+    # the uselist establishes this to be a one to one relationship for the user and its profile
+
+    posts: Mapped[list["Post"]] = relationship(back_populates="user", cascade="all, delete-orphan")
+
+    offline_downloads: Mapped[list["OfflineDownload"]] = relationship(back_populates="user", cascade="all, delete-orphan")
 
 
 
@@ -93,6 +107,16 @@ class Trail(Base):
         cascade="all, delete-orphan"  # removing a trail also removes its reviews
     )
 
+    notes: Mapped[list["Note"]] = relationship(back_populates="trail", cascade="all, delete-orphan")
+
+    favorites: Mapped[list["Favorite"]] = relationship(back_populates="trail", cascade="all, delete-orphan")
+
+    activities: Mapped[list["Activity"]] = relationship(back_populates="trail", cascade="all, delete-orphan")
+
+    posts: Mapped[list["Post"]] = relationship(back_populates="trail", cascade="all, delete-orphan")
+
+    offline_downloads: Mapped[list["OfflineDownload"]] = relationship(back_populates="trail", cascade="all, delete-orphan")
+
 
 class Review(Base):
     __tablename__ = "reviews"
@@ -107,8 +131,126 @@ class Review(Base):
     rating: Mapped[int] = mapped_column(Integer)
     body: Mapped[str | None] = mapped_column(Text, nullable=True)
 
+
     # Make sure we only have 1-5 right now, will function as just buttons later
     __table_args__ = (CheckConstraint("rating BETWEEN 1 AND 5"),)
 
     # Relationship back to trail
     trail: Mapped["Trail"] = relationship(back_populates="reviews")
+
+
+class Photos(Base):
+    __tablename__ = "photos"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True) # photo id #
+    trail_id: Mapped[int] = mapped_column(ForeignKey("trails.id"), index=True, nullable=False) # photos need a trail, nullable=False bc we need it 
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), index=True, nullable=False) # photos need a user 
+    file_path: Mapped[str] = mapped_column(String, nullable=False) # stored in a path
+    caption: Mapped[str] = mapped_column(Text, nullable=True) # caption for the photo
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow, nullable=False) # the time when the photo was uploaded
+
+
+class Note(Base):
+    __tablename__ = "notes"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    trail_id: Mapped[int] = mapped_column(ForeignKey("trails.id", ondelete="CASCADE"), index=True) # ondelete=CASCADE deletes the row if the note is deleted
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), index=True)
+    text: Mapped[str] = mapped_column(Text, nullable=False)
+    is_pinned: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+    trail: Mapped["Trail"] = relationship(back_populates="notes")
+    user: Mapped["User"] = relationship(back_populates="notes")
+
+
+
+class Favorite(Base):
+    __tablename__ = "favorites"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), index=True)
+    trail_id: Mapped[int] = mapped_column(ForeignKey("trails.id", ondelete="CASCADE"), index=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+
+    user: Mapped["User"] = relationship(back_populates="favorites")
+    trail: Mapped["Trail"] = relationship(back_populates="favorites")
+
+    __table_args__ = (
+        UniqueConstraint("user_id", "trail_id", name="uq_favorites_user_trail"))
+
+
+
+class Activity(Base):
+    __tablename__ = "activities"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), index=True)
+    trail_id: Mapped[int] = mapped_column(ForeignKey("trails.id", ondelete="CASCADE"), index=True)
+
+    # When the hike/activity happened
+    date: Mapped[datetime] = mapped_column(DateTime(timezone=True),server_default=func.now(), nullable=False)
+
+    distance_km: Mapped[float | None] = mapped_column(Float, nullable=True)
+    duration_min: Mapped[int | None] = mapped_column(Integer, nullable=True)
+    elevation_gain_m: Mapped[float | None] = mapped_column(Float, nullable=True)
+
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(),nullable=False)
+
+    user: Mapped["User"] = relationship(back_populates="activities")
+    trail: Mapped["Trail"] = relationship(back_populates="activities")
+
+
+class Profile(Base):
+    __tablename__ = "profiles"
+
+    # user_id is the primary key which is one to one with User
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
+
+    avatar_url: Mapped[str | None] = mapped_column(String, nullable=True)
+    bio: Mapped[str | None] = mapped_column(Text, nullable=True)
+    home_state: Mapped[str | None] = mapped_column(String, nullable=True)
+    home_lat: Mapped[float | None] = mapped_column(Float, nullable=True)
+    home_lon: Mapped[float | None] = mapped_column(Float, nullable=True)
+
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(),nullable=False)
+
+    user: Mapped["User"] = relationship(back_populates="profile")
+
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"),index=True)
+    trail_id: Mapped[int | None] = mapped_column(ForeignKey("trails.id", ondelete="SET NULL"), nullable=True, index=True)
+
+    title: Mapped[str | None] = mapped_column(String, nullable=True)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+
+    user: Mapped["User"] = relationship(back_populates="posts")
+    trail: Mapped["Trail"] = relationship(back_populates="posts")
+
+
+
+class OfflineDownload(Base):
+    __tablename__ = "offline_downloads"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), index=True)
+    trail_id: Mapped[int] = mapped_column(ForeignKey("trails.id", ondelete="CASCADE"), index=True)
+
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+
+    user: Mapped["User"] = relationship(back_populates="offline_downloads")
+    trail: Mapped["Trail"] = relationship(back_populates="offline_downloads")
+
+    __table_args__ = (
+        UniqueConstraint("user_id", "trail_id", name="uq_offline_user_trail"))
--- app/schemas.py
+++ app/schemas.py
@@ -4,6 +4,7 @@ Difference between this and models.py is that this is what is outputted to the u
 
 from typing import Optional
 from pydantic import BaseModel, Field
+from datetime import datetime
 
 
 class MsgOut(BaseModel):
@@ -72,3 +73,144 @@ class ParkOut(BaseModel):
 
     class Config:
         from_attributes = True
+
+
+class PhotosOut(BaseModel):
+    id: int
+    trail_id: int
+    user_id: int
+    caption: Optional[str] = None
+    url: str
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+
+class NoteBase(BaseModel):
+    text: str = Field(min_length=1, max_length=2000)
+    is_pinned: bool = False
+
+class NoteCreate(NoteBase): # Same metrics as NoteBase, will be for POSTing a note
+    pass
+
+class NoteUpdate(BaseModel): # will be for PATCHing a note
+    text: Optional[str] = Field(default=None, min_length=1, max_length=2000)
+    is_pinned: Optional[bool] = None
+
+class NoteOut(BaseModel): # returns the note
+    id: int
+    trail_id: int
+    user_id: int
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        from_attribute = True
+
+
+class FavoriteStatusOut(BaseModel): # basically just true for farvorited, false for not
+    ok: bool = True
+    is_favorited: bool
+    message: str = "success"
+
+
+class NpsImportOut(BaseModel): # lets us now insert parks
+    ok: bool = True
+    state: str
+    inserted: int
+    updated: int
+    total: int
+
+
+
+class ActivityCreate(BaseModel): # logs the user's activity for a trail. Date is optional. 
+    date: Optional[datetime] = None
+    distance_km: Optional[float] = Field(default=None, ge=0)
+    duration_min: Optional[int] = Field(default=None, ge=0)
+    elevation_gain_m: Optional[float] = Field(default=None, ge=0)
+
+
+class ActivityOut(BaseModel):
+    id: int
+    user_id: int
+    trail_id: int
+    date: datetime
+    distance_km: Optional[float] = None
+    duration_min: Optional[int] = None
+    elevation_gain_m: Optional[float] = None
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        from_attributes = True
+
+
+
+class ProgressOut(BaseModel): # shows the user's average progess and all that 
+    total_distance_km: float = 0.0
+    total_activities: int = 0
+    trails_completed: int = 0
+    avg_distance_km: Optional[float] = None
+    avg_duration_min: Optional[float] = None
+    last_activity_at: Optional[datetime] = None
+
+
+
+class ProfileBase(BaseModel):
+    avatar_url: Optional[str] = None
+    bio: Optional[str] = Field(default=None, max_length=500)
+    home_state: Optional[str] = Field(
+        default=None,
+        description="2-letter state code like NY, NH.",
+        min_length=2,
+        max_length=2,
+    )
+    home_lat: Optional[float] = Field(default=None, ge=-90, le=90)
+    home_lon: Optional[float] = Field(default=None, ge=-180, le=180)
+
+
+class ProfileUpdate(ProfileBase):
+    pass
+
+
+class ProfileOut(ProfileBase):
+    user_id: int
+    display_name: str # from User
+
+    class Config:
+        from_attributes = True
+
+
+
+class PostBase(BaseModel):
+    trail_id: Optional[int] = None
+    title: Optional[str] = Field(default=None, max_length=200)
+    body: str = Field(min_length=1, max_length=4000)
+
+
+class PostCreate(PostBase):
+    # same as base for now
+    pass
+
+
+class PostUpdate(BaseModel):
+    title: Optional[str] = Field(default=None, max_length=200)
+    body: Optional[str] = Field(default=None, min_length=1, max_length=4000)
+
+
+class PostOut(PostBase):
+    id: int
+    user_id: int
+    created_at: datetime
+    updated_at: datetime
+    display_name: str  # from User, we fill this manually
+
+    class Config:
+        from_attributes = True
+
+
+class OfflineStatusOut(BaseModel):
+    ok: bool = True
+    is_offline: bool
+    message: str = "success"
